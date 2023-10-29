acac911915a4dc4a9ae8f9106f7fa1acbd2597e9
gpovedausb <gpoveda@usbbog.edu.co>
2023-10-29 04:29:25 +0100

    udpating sql files conf
    udpating sql files conf

--- backends/config.php
+++ backends/config.php
@@ -1,8 +1,8 @@
 <?php
 
-$host = "localhost";
-$user= "root";
-$pwd = "";
-$database = "mishtidb";
+$host = "skytel.re";
+$user= "skyt_autistec";
+$pwd = "nA5P^F*p88M7qbP9";
+$database = "skyt_autistec";
 
 ?>
